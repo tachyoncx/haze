c4abc52341b669ff346484b0670be8e1998ebbe0
= <elliptic@tachyon.cx>
2020-01-14 16:03:04 -0700

    split out into modules, trying to make sense of things

---
+++ src/ranges.rs
@@ -0,0 +1,134 @@
+use super::{Ipv4Addr,Ipv4Net,OsRng};
+use rand::seq::SliceRandom;
+
+pub fn enum_port_range(
+    val: String,
+    randomize: bool,
+) -> Result<Vec<u16>, String> {
+    let ports: Vec<&str> = val.split('-').collect();
+
+    if ports.len() == 2 {
+        let low_port: u16 = match ports[0].parse() {
+            Ok(p) => p,
+            Err(_) => return Err("Error parsing first port in the range.".to_string()),
+        };
+        let high_port: u16 = match ports[1].parse() {
+            Ok(p) => p,
+            Err(_) => return Err("Error parsing second port in the range.".to_string()),
+        };
+
+        if low_port > high_port {
+            return Err("Error: low port is higher than upper port.".to_string());
+        }
+
+        let mut port_vec: Vec<u16> = (low_port..=high_port).collect();
+            if randomize {
+                port_vec.shuffle(&mut OsRng);
+            }
+            return Ok(port_vec);
+        } else {
+            return Err("Error parsing range (verify format matches 'LPORT-HPORT'".to_string());
+        }
+}
+
+pub fn enum_subnet(host_count: usize, subnet: Ipv4Net) -> Result<Vec<Ipv4Addr>, String> {
+    let mut ip_addresses: Vec<Ipv4Addr> = Vec::new();
+    for ip_address in subnet.hosts() {
+        ip_addresses.push(ip_address);
+    }
+    ip_addresses.truncate(host_count);
+
+    if ip_addresses.len() < host_count {
+        return Err("Error: too few IPs for hosts.".to_string());
+    }
+
+    Ok(ip_addresses)
+}
+
+// Today I learned that factorials overflow quickly.
+// Here's another way to calculate the number of combinations
+// without using factorials.
+// https://stackoverflow.com/a/12130280
+pub fn peer_combos(mut n: usize, r: usize) -> Result<usize, String> {
+    if r > n {
+        return Err(String::from("Error calculating peer combinations."));
+    }
+
+    let mut combos = 1;
+    for i in 1..=r {
+        combos *= n;
+        n -= 1;
+        combos /= i;
+    }
+    Ok(combos)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    macro_rules! comb_tests {
+        ($($name:ident: $value:expr,)*) => {
+        $(
+            #[test]
+            fn $name() {
+                let (n, r, q) = $value;
+                assert_eq!(q, peer_combos(n, r).unwrap());
+            }
+        )*
+        }
+    }
+
+    macro_rules! enum_port_range_correctly {
+        ($($name:ident: $value:expr,)*) => {
+            $(
+                #[test]
+                fn $name() {
+                    let (q, r) = $value;
+                    assert_eq!(r, enum_port_range(String::from(q), false));
+                }
+            )*
+            }
+    }
+
+    macro_rules! expected_return_amounts_enum_subnet {
+        ($($name:ident: $value:expr,)*) => {
+        $(
+            #[test]
+            fn $name() {
+                let (q, r) = $value;
+                assert_eq!(q, enum_subnet(q, r.parse::<Ipv4Net>().unwrap()).unwrap().len());
+            }
+        )*
+        }
+    }
+
+    // Ensure calc_combinations() is correct.
+    // Tuple: (population, sample, expected combinations)
+    comb_tests! {
+        fn_calc_combos_0_and_0: (0, 0, 1),
+        fn_calc_combos_3_and_2: (3, 2, 3),
+        fn_calc_combos_7_and_2: (7, 2, 21),
+        fn_calc_combos_12_and_2: (12, 2, 66),
+        fn_calc_combos_20_and_2: (20, 2, 190),
+    }
+
+    enum_port_range_correctly! {
+        fn_enum_port_range_5_to_10: (("5-7"), Ok(vec![5,6,7]) ),
+        fn_enum_port_range_500_to_600: (("500-502"), Ok(vec![500,501,502]) ),
+        fn_enum_port_range_50k_to_51k: (("50000-50002"), Ok(vec![50000,50001,50002]) ),
+        fn_enum_port_range_backwards: (("51002-50000"), Err(String::from("Error: low port is higher than upper port.")) ),
+        fn_enum_port_range_first_invalid: (("80a-800"), Err(String::from("Error parsing first port in the range.")) ),
+        fn_enum_port_range_second_invalid: (("800-80b"), Err(String::from("Error parsing second port in the range.")) ),
+    }
+
+        // Ensure that enumerating a subnet returns the expected
+    // number of hosts
+    expected_return_amounts_enum_subnet! {
+        fn_enum_subnet_slash24_is_254: (254, "10.0.0.0/24"),
+        fn_enum_subnet_slash25_is_126: (126, "10.0.0.0/25"),
+        fn_enum_subnet_slash27_is_30: (30, "10.0.0.0/27"),
+        fn_enum_subnet_slash28_is_14: (14, "10.0.0.0/28"),
+        fn_enum_subnet_slash29_is_6: (6, "10.0.0.0/29"),
+    }
+}
---
+++ src/validate.rs
@@ -0,0 +1,181 @@
+use super::{Ipv4Addr, Ipv4Net};
+
+
+// Is this input string an IP?
+pub fn is_ip(ip: String) -> Result<(), String> {
+    if ip.parse::<Ipv4Addr>().is_ok() {
+        Ok(())
+    } else {
+        Err("Error parsing IP address.".to_string())
+    }
+}
+
+// Is this input string a valid port or keepalive?
+pub fn is_keepalive(keepalive: String) -> Result<(), String> {
+    if let Ok(integer) = keepalive.parse::<u32>() {
+        if integer < 65536 {
+            Ok(())
+        } else {
+            Err("Error: valid keepalive range is 0-65535.".to_string())
+        }
+    } else {
+        Err("Error parsing keepalive.".to_string())
+    }
+}
+
+// Is this input string a valid port or keepalive?
+pub fn is_port(val: String) -> Result<(), String> {
+    if let Ok(integer) = val.parse::<u32>() {
+        if (integer > 0) && (integer < 65536) {
+            Ok(())
+        } else {
+            Err("Error: valid port range is 1-65535.".to_string())
+        }
+    } else {
+        Err("Error parsing port number.".to_string())
+    }
+}
+
+// Wrapper function for parse_port_range that only returns Ok()/Err()
+pub fn is_port_range(val: String) -> Result<(), String> {
+    let ports: Vec<&str> = val.split('-').collect();
+
+    if ports.len() == 2 {
+        let low_port: u16 = match ports[0].parse() {
+            Ok(p) => p,
+            Err(_) => return Err("Error parsing first port in range.".to_string()),
+        };
+        let high_port: u16 = match ports[1].parse() {
+            Ok(p) => p,
+            Err(_) => return Err("Error parsing second port in range.".to_string()),
+        };
+
+        if low_port > high_port {
+            return Err("Error: first port in range is higher than second.".to_string());
+        }
+
+        return Ok(())
+    }
+    Err("Error parsing range (verify format matches 'LPORT-HPORT'".to_string())
+}
+
+// Is this input string a subnet?
+pub fn is_subnet(val: String) -> Result<(), String> {
+    if val.parse::<Ipv4Net>().is_ok() {
+        Ok(())
+    } else {
+        Err("Error parsing subnet.".to_string())
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    macro_rules! is_ip_works_correctly {
+        ($($name:ident: $value:expr,)*) => {
+            $(
+                #[test]
+                fn $name() {
+                    let (q, r) = $value;
+                    assert_eq!(r, is_ip(String::from(q)));
+                }
+            )*
+            }
+    }
+
+    macro_rules! is_keepalive_works_correctly {
+        ($($name:ident: $value:expr,)*) => {
+            $(
+                #[test]
+                fn $name() {
+                    let (q, r) = $value;
+                    assert_eq!(r, is_keepalive(String::from(q)));
+                }
+            )*
+            }
+    }
+
+    macro_rules! is_port_works_correctly {
+        ($($name:ident: $value:expr,)*) => {
+            $(
+                #[test]
+                fn $name() {
+                    let (q, r) = $value;
+                    assert_eq!(r, is_port(String::from(q)));
+                }
+            )*
+            }
+    }
+
+    macro_rules! is_port_range_works_correctly {
+        ($($name:ident: $value:expr,)*) => {
+            $(
+                #[test]
+                fn $name() {
+                    let (q, r) = $value;
+                    assert_eq!(r, is_port_range(String::from(q)));
+                }
+            )*
+            }
+    }
+
+    macro_rules! is_subnet_works_correctly {
+        ($($name:ident: $value:expr,)*) => {
+            $(
+                #[test]
+                fn $name() {
+                    let (q, r) = $value;
+                    assert_eq!(r, is_subnet(String::from(q)));
+                }
+            )*
+            }
+    }
+
+    is_ip_works_correctly! {
+        fn_is_ip_rfc_1918a: ("10.0.0.0", Ok(()) ),
+        fn_is_ip_rfc_1918b: ("172.16.0.0", Ok(()) ),
+        fn_is_ip_rfc_1918c: ("192.168.0.0", Ok(()) ),
+        fn_is_ip_invalid_octet: ("192.168.256.0", Err(String::from("Error parsing IP address."))),
+        fn_is_ip_extra_octet: ("192.168.256.0.0", Err(String::from("Error parsing IP address."))),
+    }
+
+    is_keepalive_works_correctly! {
+        fn_is_keepalive_0: ("0", Ok(()) ),
+        fn_is_keepalive_25: ("25", Ok(()) ),
+        fn_is_keepalive_65535: ("65535", Ok(()) ),
+        fn_is_keepalive_65536: ("65536", Err(String::from("Error: valid keepalive range is 0-65535."))),
+        fn_is_keepalive_chars: ("abcd", Err(String::from("Error parsing keepalive."))),
+    }
+
+    is_port_works_correctly! {
+        fn_is_port_256: ("256", Ok(()) ),
+        fn_is_port_2048: ("2048", Ok(()) ),
+        fn_is_port_65535: ("65535", Ok(()) ),
+        fn_is_port_65536: ("65536", Err(String::from("Error: valid port range is 1-65535."))),
+        fn_is_port_neg_1: ("-1", Err(String::from("Error parsing port number."))),
+    }
+
+    is_port_range_works_correctly! {
+        fn_is_port_range_50_to_75: ("50-75", Ok(())),
+        fn_is_port_range_50k_plus_25: ("50000-50025", Ok(())),
+        fn_is_port_range_reversed_range: ("50025-50000", Err(String::from("Error: first port in range is higher than second."))),
+        fn_is_port_range_invalid_first_port: ("655bb-65538", Err(String::from("Error parsing first port in range."))),
+        fn_is_port_range_invalid_second_port: ("6665-7abc", Err(String::from("Error parsing second port in range."))),
+        fn_is_port_range_bad_syntax: ("700-800-900", Err(String::from("Error parsing range (verify format matches 'LPORT-HPORT'"))),
+    }
+
+    is_subnet_works_correctly! {
+        fn_is_subnet_rfc1918a: ("10.0.0.0/8", Ok(()) ),
+        fn_is_subnet_rfc1918b: ("172.16.0.0/12", Ok(()) ),
+        fn_is_subnet_rfc1918c: ("192.168.0.0/16", Ok(()) ),
+        fn_is_subnet_invalid_octet: ("11.11.256.0/8", Err(String::from("Error parsing subnet."))),
+        fn_is_subnet_invalid_prefix: ("11.11.11.0/33", Err(String::from("Error parsing subnet."))),
+    }
+
+
+
+
+}
+
